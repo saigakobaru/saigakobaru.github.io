631b6ad38fa8b196af17d9837dbd5de44e8a47b2
saigakobaru <50170606+saigakobaru@users.noreply.github.com>
2019-05-03 22:51:09 +0900

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+// JavaScript Document$
+
+
+$(function() {
+  $('#move_target').click(function() {
+    // スクロール
+    $('html,body').animate({scrollTop: 0}, 800, 'swing');
+  });
+});
+
+
+
+$(function() {
+  $('#move').click(function() {
+    // スクロール
+    $('html,body').animate({scrollTop: 2500}, 800, 'swing');
+  });
+});
+
